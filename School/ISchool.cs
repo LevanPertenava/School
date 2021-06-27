5fb3a5f638d6023f4e82cc82c1fe875f3f52c579
LevanPertenava <levan.pertenava1@gmail.com>
2021-06-27 12:46:56 +0400

    Add project files.

---
+++ School/ISchool.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace School
+{
+    interface ISchool
+    {
+        void AddPerson(string name);
+
+        void SetPoint(int personID, int grade);
+
+        void RemovePerson(int personID);
+
+        int GetPersonPoint(int personID);
+    }
+}
---
+++ School/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace School
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StudentDictionary studentDictionary = new();
+            studentDictionary.AddPerson("levani");
+            studentDictionary.AddPerson("nika");
+            studentDictionary.SetPoint(2, 100);
+
+            foreach (var item in studentDictionary.Students)
+            {
+                Console.WriteLine($"Student Name : {item.Value.Name}, Grade : {item.Value.Grade}");
+            }
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Grade { get; set; }
+    }
+
+    public class StudentDictionary : ISchool
+    {
+        public Dictionary<int, Person> Students { get; private set; }
+
+        public StudentDictionary()
+        {
+            Students = new Dictionary<int, Person>();
+        }
+        public void AddPerson(string name)
+        {
+            Students.Add(Students.Count + 1, new Person() { Name = name });
+        }
+
+        public int GetPersonPoint(int personID)
+        {
+            return Students[personID].Grade;
+        }
+
+        public void RemovePerson(int personID)
+        {
+            if (!Students.ContainsKey(personID))
+            {
+                throw new Exception("No student was found by this ID");
+            }
+            Students.Remove(personID);
+        }
+
+        public void SetPoint(int personID, int grade)
+        {
+            Students[personID].Grade = grade;
+        }
+    }
+}
